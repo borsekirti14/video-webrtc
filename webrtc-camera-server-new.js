b220a800c316f12b60e2ad7fa286c2ed9da60667
Kirti Jadhav <borsekirti14@gmail.com>
2026-09-23 09:55:17 +0530

    stun server changes

--- webrtc-camera-server-new.js
+++ webrtc-camera-server-new.js
@@ -157,24 +157,25 @@ const FRAME_SIZE =
  * ------------------------------------------------ */
 
 const ICE_SERVERS = [
-    {
-        urls: [
-            "stun:stun.cloudflare.com:3478"
-        ]
-    },
-    {
-        urls: [
-            "stun:stun.l.google.com:19302",
-            "stun1.l.google.com:19302",
-            "stun2.l.google.com:19302",
-            "stun3.l.google.com:19302"
-        ]
-    },
-    {
-        urls: [
-            "stun:stun.sipgate.net:3478"
-        ]
-    }
+  {
+    urls: [
+      "stun:stun.cloudflare.com:3478"
+    ]
+  },
+  {
+    urls: [
+      "stun:stun.l.google.com:19302",
+      "stun:stun1.l.google.com:19302",
+      "stun:stun2.l.google.com:19302",
+      "stun:stun3.l.google.com:19302",
+      "stun:stun4.l.google.com:19302"
+    ]
+  },
+  {
+    urls: [
+      "stun:stun.sipgate.net:3478"
+    ]
+  }
 ];
 
 /* ================================================================
